3002ab8b37a26affd1d6b289a7d2d93879a4f92b
le thanh son <lethanhson1504@gmail.com>
2021-11-13 18:28:26 +0700

    complete join class by link

--- src/routers/classroom.js
+++ src/routers/classroom.js
@@ -3,6 +3,8 @@ const ClassRoom = require('../model/classroom')
 const router = new express.Router()
 const auth = require('../middleware/auth')
 const { nanoid } = require('nanoid')
+const User = require('../model/user')
+const ObjectID = require('mongodb').ObjectID
 
 //create new classroom
 router.post('/create-classroom', auth, async (req, res) => {
@@ -41,13 +43,50 @@ router.get('/classrooms', auth, async (req, res) => {
     }
 })
 
+//get list students and teachers in a class
+router.post('/students-teachers', auth, async (req, res) => {
+    const userId = req.user._id
+    
+    try {
+        const classroom = await ClassRoom.findById(req.body.classroomId)
+
+        if (classroom.teachers.toString().includes(userId) || classroom.students.toString().includes(userId)) {
+            const teachers = []
+            for (let i = 0; i < classroom.teachers.length; i++) {
+                const user = await User.findById( classroom.teachers[i] )
+                teachers.push( {name: user.name} )
+            }
+
+            const students = []
+            for (let i = 0; i < classroom.students.length; i++) {
+                const user = await User.findById( classroom.students[i] )
+                students.push( {name: user.name} )
+            }
+
+            res.status(200).send( {teachers, students} )
+        }
+        else {
+            res.status(400).send( {Error: "You don't have permission to do this!"} )
+        }
+    }
+    catch(e) { 
+        console.log(e)
+        res.status(400).send(e)
+    }
+})
+
 //invite to classroom by link
 router.get('/classrooms/:classCode', auth, async (req, res) => {
     const userId = req.user._id
 
     try {
+        const classroom = await ClassRoom.findById(req.params.classCode)
 
-        res.status(200).send(req.params.classCode)
+        if (!classroom.teachers.toString().includes(userId) && !classroom.students.toString().includes(userId)) {
+            classroom.students = classroom.students.concat( userId )
+            await classroom.save()
+        }
+        res.status(200).send(classroom)
     }
     catch(e) { 
         console.log(e)
