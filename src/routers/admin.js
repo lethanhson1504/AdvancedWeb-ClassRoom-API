6679b7d3c991622e9dba48c1206ecbd21c44c94e
Son Le <lethanhson1504@gmail.com>
2021-12-28 17:11:40 +0700

    create admin and return list admin

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -1,34 +1,51 @@
 const express = require("express");
 const Admin = require("../model/admin");
-const ClassRoom = require("../model/classroom");
 const router = new express.Router();
-const auth = require("../middleware/auth");
-const multer = require("multer");
-const sharp = require("sharp");
+const { authAdmin } = require("../middleware/auth");
 
 //create new admin
-router.post("/admin", auth.authAdmin, async (req, res) => {
-    try {
-      const duplicatedAccount = await Admin.findOne({ account: req.body.account });
-      if (duplicatedAccount) {
-        return res.status(400).send({ error: "This account has been registered!" });
-      }
-  
-      const admin = new Admin(req.body);
-  
-      const token = await admin.generateAuthToken();
-  
-      res.status(201).send({ admin, token });
-    } catch (e) {
-      console.log(e);
-      res.status(400).send(e);
+router.post("/admin", async (req, res) => {
+    const secretCode = req.body.secretCode
+    if (secretCode !== process.env.ADMIN_SECRET_CODE) {
+        return res.status(400).send({ error: "Wrong secret code!" });
     }
-  });
 
-//get 1 users with authorization
-router.get("/admin/me", auth.authAdmin, async (req, res) => {
-    res.send(req.admin);
-  });
+    try {
+        const duplicatedAccount = await Admin.findOne({ account: req.body.account });
+        if (duplicatedAccount) {
+            return res.status(400).send({ error: "This account has been registered!" });
+        }
 
-  module.exports = router
-  
+        const admin = new Admin(req.body);
+
+        const token = await admin.generateAuthToken();
+
+        res.status(201).send({ admin, token });
+    } catch (e) {
+        console.log(e);
+        res.status(400).send(e);
+    }
+});
+
+//login user
+router.post("/admin/login", async (req, res) => {
+    try {
+        const admin = await Admin.findByCredentials(
+            req.body.account,
+            req.body.password
+        );
+
+        token = await admin.generateAuthToken();
+
+        res.send({ admin, token });
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+router.get("/admins", authAdmin, async (req, res) => {
+    const listAdmin = await Admin.find({}).sort({ createdAt: req.query.createdAt})
+    res.status(200).send(listAdmin)
+})
+
+module.exports = router
