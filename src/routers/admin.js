e06bc6b7969cc64a85f9ab4ae0ebe2509421a681
Son Le <lethanhson1504@gmail.com>
2022-01-09 10:11:42 +0700

    update lock and ban user

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -184,6 +184,7 @@ router.get("/admin/user/lock-account", authAdmin, (req, res) => {
             }
 
             result.status = 'lock'
+            result.tokens = [];
             result.save();
             return res.status(200).send("Lock acount success!")
         })
@@ -226,6 +227,8 @@ router.get("/admin/user/ban-account", authAdmin, (req, res) => {
             result.status = 'ban'
             result.name = 'Ban account'
             result.studentId = 'Ban account'
+            result.tokens = [];
+
             result.save();
             return res.status(200).send("Ban acount success!")
         })
